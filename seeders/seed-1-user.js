bb8a986840510d7be39050f8494cf479c2f9467d
nutchapon <nutchapon.han@gmail.com>
2020-01-14 17:08:44 +0700

    mockup store data

--- seeders/seed-1-user.js
+++ seeders/seed-1-user.js
@@ -12,7 +12,8 @@ module.exports = {
         phone: '0998887771',
         profile_image_url: 'https://img.icons8.com/plasticine/2x/user.png',
         role: 'user',
-        
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         password: 'sompongpass',
@@ -21,7 +22,9 @@ module.exports = {
         email: 'sompong@gmail.com',
         phone: '0998887772',
         profile_image_url: 'https://icons-for-free.com/iconfiles/png/512/casual+male+man+person+user+icon-1320196226197493271.png',
-        role: 'user'
+        role: 'user',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         password: 'kayanpass',
@@ -30,7 +33,9 @@ module.exports = {
         email: 'kayanpass@gmail.com',
         phone: '0998887773',
         profile_image_url: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Wikipedia_User-ICON_byNightsight.png',
-        role: 'user'
+        role: 'user',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         password: 'rangdeepass',
@@ -39,7 +44,9 @@ module.exports = {
         email: 'rangdee@gmail.com',
         phone: '0998887774',
         profile_image_url: 'https://cdn1.iconfinder.com/data/icons/character-2/107/8-512.png',
-        role: 'user'
+        role: 'user',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         password: 'jintalapass',
@@ -48,7 +55,9 @@ module.exports = {
         email: 'jintala@gmail.com',
         phone: '0998887775',
         profile_image_url: 'https://cdn1.iconfinder.com/data/icons/character-2/240/19-512.png',
-        role: 'user'
+        role: 'user',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         password: 'chateepass',
@@ -57,7 +66,9 @@ module.exports = {
         email: 'chatee@gmail.com',
         phone: '0998887776',
         profile_image_url: 'https://cdn3.vectorstock.com/i/1000x1000/44/02/happy-young-man-smile-profile-cartoon-character-vector-18864402.jpg',
-        role: 'user'
+        role: 'user',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         password: 'sekpass',
@@ -66,7 +77,9 @@ module.exports = {
         email: 'sek@gmail.com',
         phone: '0998887777',
         profile_image_url: 'https://cdn2.vectorstock.com/i/1000x1000/28/96/young-man-face-cartoon-design-vector-9772896.jpg',
-        role: 'user'
+        role: 'user',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         password: 'ponlawatpass',
@@ -75,7 +88,9 @@ module.exports = {
         email: 'ponlawat@gmail.com',
         phone: '0998887778',
         profile_image_url: 'https://cdn3.vectorstock.com/i/1000x1000/90/17/indian-man-face-avatar-cartoon-vector-25919017.jpg',
-        role: 'user'
+        role: 'user',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         password: 'jamepass',
@@ -84,7 +99,9 @@ module.exports = {
         email: 'jame@gmail.com',
         phone: '0998887779',
         profile_image_url: 'https://making-the-web.com/sites/default/files/clipart/148583/cartoon-man-face-148583-7316849.jpg',
-        role: 'user'
+        role: 'user',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         password: 'ponraapass',
@@ -93,7 +110,9 @@ module.exports = {
         email: 'ponraa@gmail.com',
         phone: '0998887780',
         profile_image_url: 'https://cdn1.vectorstock.com/i/1000x1000/06/45/man-face-pop-art-cartoon-vector-19080645.jpg',
-        role: 'user'
+        role: 'user',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         password: 'hansapass',
@@ -102,16 +121,20 @@ module.exports = {
         email: 'hansa@gmail.com',
         phone: '0998887781',
         profile_image_url: 'https://previews.123rf.com/images/farhad73/farhad731807/farhad73180700006/104271850-man-cartoon-face-with-glasses-vector-illustration-.jpg',
-        role: 'user'
+        role: 'user',
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         password: 'saadpass',
         firstname: 'สะอาด',
         lastname: 'จริงจริงนะ',
         email: 'saad@gmail.com',
-        phone: '0998887779',
+        phone: '0998887782',
         profile_image_url: 'https://cdn4.vectorstock.com/i/1000x1000/77/43/young-man-head-avatar-cartoon-face-character-vector-21757743.jpg',
-        role: 'user'
+        role: 'user',
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
     ])
   },
