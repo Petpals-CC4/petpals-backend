a2af99e15e6e65bb0307de506a58b3da6aa0657a
NAPAS KAVALEE <napas.kavalee@gmail.com>
2020-01-22 11:42:24 +0700

    [Edited] - get bank

--- services/bank.js
+++ services/bank.js
@@ -1,22 +1,33 @@
 module.exports = (app, db) => {
   
-  app.get("/bank/:store_id", (req, res) => {
-    db.store
-      .findOne({
-        where: {
-          id: req.params.store_id
-        },
-        attributes: [],
-        include: [
-            {
-            model: db.bank,
-            attributes:['id', 'bank_name', 'account_name','account_number']
-            }
-          ]
-        })
-        .then(result => {
-        res.send(result);
-      });
+  app.get("/bank/:store_id", async (req, res) => {
+    let result = await db.bank.findAll({
+      where: {
+        store_id: req.params.store_id
+      }
+    })
+    if (!result) {
+      res.status(404).send({message: "Not found Bank by store id"})
+    } else {
+      res.status(200).send(result)
+    }
+
+    // db.store
+    //   .findOne({
+    //     where: {
+    //       id: req.params.store_id
+    //     },
+    //     attributes: [],
+    //     include: [
+    //         {
+    //         model: db.bank,
+    //         attributes:['id', 'bank_name', 'account_name','account_number']
+    //         }
+    //       ]
+    //     })
+    //     .then(result => {
+    //     res.send(result);
+    //   });
   });
 }
 
