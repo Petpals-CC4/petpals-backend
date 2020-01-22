aa485bbc28758c62f2d933aef9f296a5664b5917
ChayutMill <chayut.rod@gmail.com>
2020-01-22 16:26:13 +0700

    [Added] - service/bank - (PUT and DELETE)

--- services/bank.js
+++ services/bank.js
@@ -1,15 +1,14 @@
 module.exports = (app, db) => {
-  
   app.get("/bank/:store_id", async (req, res) => {
     let result = await db.bank.findAll({
       where: {
         store_id: req.params.store_id
       }
-    })
+    });
     if (!result) {
-      res.status(404).send({message: "Not found Bank by store id"})
+      res.status(404).send({ message: "Not found Bank by store id" });
     } else {
-      res.status(200).send(result)
+      res.status(200).send(result);
     }
 
     // db.store
@@ -48,5 +47,50 @@ module.exports = (app, db) => {
           message: err.message
         });
       });
+  });
+
+  app.put("/bank/:id", async (req, res) => {
+    const id = req.params.id;
+    const store_id = req.body.store_id;
+    console.log(
+      "store_idstore_idstore_idstore_idstore_idstore_idstore_idstore_id",
+      store_id
+    );
+    const bankFound = await db.bank.findOne({
+      where: { store_id, id }
+    });
+    if (!bankFound) {
+      res.status(404).send({ message: "Error: Not Found" });
+    } else {
+      try {
+        const bank = await bankFound.update({
+          bank_name: req.body.bank_name,
+          account_name: req.body.account_name,
+          account_number: req.body.account_number,
+          store_id: req.body.store_id
+        });
+        console.log(bank);
+        res.status(200).send({ message: "Update Success", ...bank.dataValues });
+      } catch (error) {
+        // res.status(400).send({ message: "bank name cannot be empty." })
+        res.status(400).send({ message: error.errors[0].message });
+      }
+    }
+  });
+
+  app.delete("/bank/:id", async (req, res) => {
+    const id = req.params.id;
+    const store_id = req.body.store_id;
+
+    const bankFound = await db.bank.findOne({
+      where: { store_id, id }
+    });
+    if (!bankFound) {
+      res.status(404).send({ message: "Error: Not Found" });
+    } else {
+      const wallet = await bankFound.destroy();
+      console.log(wallet);
+      res.status(200).send({ message: "Delete Success" });
+    }
   });
 };
