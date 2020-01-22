41fad1b063ec849462af250e1547b7a3a8b34e9f
ChayutMill <chayut.rod@gmail.com>
2020-01-22 11:22:53 +0700

    [Added] - service/bank - POST

--- services/bank.js
+++ services/bank.js
@@ -1,5 +1,4 @@
 module.exports = (app, db) => {
-  
   app.get("/bank/:store_id", (req, res) => {
     db.store
       .findOne({
@@ -8,15 +7,34 @@ module.exports = (app, db) => {
         },
         attributes: [],
         include: [
-            {
+          {
             model: db.bank,
-            attributes:['id', 'bank_name', 'account_name','account_number']
-            }
-          ]
-        })
-        .then(result => {
+            attributes: ["id", "bank_name", "account_name", "account_number"]
+          }
+        ]
+      })
+      .then(result => {
         res.send(result);
       });
   });
-}
 
+  app.post("/bank", (req, res) => {
+    let reqBody = {
+      bank_name: req.body.bank_name,
+      account_name: req.body.account_name,
+      account_number: req.body.account_number,
+      store_id: req.body.store_id
+    };
+    db.bank
+      .create(reqBody)
+      .then(result => {
+        res.status(201).json(result);
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(400).json({
+          message: err.message
+        });
+      });
+  });
+};
