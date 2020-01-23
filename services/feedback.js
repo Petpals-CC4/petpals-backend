81ef63494a155d1d44addb967ffaf43c9aa7204f
Chinnawat Chimdee <chinnawat.cpre@gmail.com>
2020-01-23 11:26:59 +0700

    [Added] - Added Feedback API - Added Postman Collection

--- services/feedback.js
+++ services/feedback.js
@@ -1,6 +1,43 @@
 const passport = require('passport');
 
 module.exports = (app, db) => {
+  app.get("/feedback_random",
+    async (req, res) => {
+      let feedbacks = await db.feedback.findAll({
+        attributes: ["id", "rating", "comment"],
+        include: [
+          {
+            model: db.store,
+            attributes: ['store_name']
+          },
+          {
+            model: db.user,
+            attributes: ['firstname', 'lastname']
+          }
+        ]
+      });
+      if (!feedbacks) {
+        res.status(404).send({ message: "Not found Feedback" });
+      } else {
+        let feedbacksLean = await feedbacks.map((feedback) => {
+          return {
+            id: feedback.id,
+            comment: feedback.comment,
+            storename: feedback.store.dataValues.store_name,
+            fullname: feedback.user.dataValues.firstname + " " + feedback.user.dataValues.lastname
+          }
+        })
+        let maxLen = feedbacksLean.length
+        let randomIndex = [];
+        while (randomIndex.length < 3 && randomIndex.length < maxLen) {
+          let randVal = Math.floor(Math.random() * maxLen)
+          if (randomIndex.indexOf(randVal) === -1) randomIndex.push(randVal)
+        }
+        res.status(200).send(feedbacksLean.filter((_, index) => randomIndex.indexOf(index) > -1));
+      }
+    }
+  )
+
   app.get("/feedback",
     passport.authenticate('jwt', { session: false }),
     async (req, res) => {
@@ -26,7 +63,7 @@ module.exports = (app, db) => {
             rating: feedback.rating,
             comment: feedback.comment,
             updatedAt: feedback.updatedAt,
-            fullname: feedback.store.dataValues.store_name
+            storeName: feedback.store.dataValues.store_name
           }
         })
         res.status(200).send(feedbacksLean);
