e10cc9a7dc6a439474bfa70b085c8e599d5e51ea
ChayutMill <chayut.rod@gmail.com>
2020-01-25 14:24:36 +0700

    [Edited] - service/address and bank

--- services/address.js
+++ services/address.js
@@ -7,15 +7,19 @@ module.exports = (app, db) => {
     passport.authenticate("jwt", { session: false }),
     async (req, res) => {
       const store_id = await findStoreIDbyUserID(db, req.user.id);
-      let result = await db.address.findAll({
-        where: {
-          store_id
+      if (store_id !== null) {
+        let result = await db.address.findAll({
+          where: {
+            store_id
+          }
+        });
+        if (!result) {
+          res.status(404).send({ message: "Not found address by store id" });
+        } else {
+          res.status(200).send(result);
         }
-      });
-      if (!result) {
-        res.status(404).send({ message: "Not found address by store id" });
       } else {
-        res.status(200).send(result);
+        res.status(401).send({ message: "Unauthorized" });
       }
     }
   );
@@ -25,8 +29,8 @@ module.exports = (app, db) => {
     passport.authenticate("jwt", { session: false }),
     async (req, res) => {
       const store_id = await findStoreIDbyUserID(db, req.user.id);
-      await db.address
-        .create({
+      if (store_id !== null) {
+        let result = await db.address.create({
           house_no: req.body.house_no,
           village_no: req.body.village_no,
           road: req.body.road,
@@ -35,16 +39,17 @@ module.exports = (app, db) => {
           province: req.body.province,
           post_code: req.body.post_code,
           store_id
-        })
-        .then(result => {
-          res.status(201).json(result);
-        })
-        .catch(err => {
-          console.error(err);
-          res.status(400).json({
-            message: err.message
-          });
         });
+        if (!result) {
+          res.status(400).json({
+            message: "Cannot Create Address, please check your body request"
+          });
+        } else {
+          res.status(201).json(result);
+        }
+      } else {
+        res.status(401).send({ message: "Unauthorized" });
+      }
     }
   );
 
@@ -54,30 +59,34 @@ module.exports = (app, db) => {
     async (req, res) => {
       const id = req.params.id;
       const store_id = await findStoreIDbyUserID(db, req.user.id);
-      const addressFound = await db.address.findOne({
-        where: { store_id, id }
-      });
-      if (!addressFound) {
-        res.status(404).send({ message: "Error: Not Found" });
-      } else {
-        try {
-          const address = await addressFound.update({
-            house_no: req.body.house_no,
-            village_no: req.body.village_no,
-            road: req.body.road,
-            sub_district: req.body.sub_district,
-            district: req.body.district,
-            province: req.body.province,
-            post_code: req.body.post_code
-          });
-          console.log(address);
-          res
-            .status(200)
-            .send({ message: "Update Success", ...address.dataValues });
-        } catch (error) {
-          // res.status(400).send({ message: "address name cannot be empty." })
-          res.status(400).send({ message: error.errors[0].message });
+      if (store_id !== null) {
+        const addressFound = await db.address.findOne({
+          where: { store_id, id }
+        });
+        if (!addressFound) {
+          res.status(404).send({ message: "Error: Not Found" });
+        } else {
+          try {
+            const address = await addressFound.update({
+              house_no: req.body.house_no,
+              village_no: req.body.village_no,
+              road: req.body.road,
+              sub_district: req.body.sub_district,
+              district: req.body.district,
+              province: req.body.province,
+              post_code: req.body.post_code
+            });
+            // console.log(address);
+            res
+              .status(200)
+              .send({ message: "Update Success", ...address.dataValues });
+          } catch (error) {
+            // res.status(400).send({ message: "address name cannot be empty." })
+            res.status(400).send({ message: error.errors[0].message });
+          }
         }
+      } else {
+        res.status(401).send({ message: "Unauthorized" });
       }
     }
   );
@@ -88,16 +97,19 @@ module.exports = (app, db) => {
     async (req, res) => {
       const id = req.params.id;
       const store_id = await findStoreIDbyUserID(db, req.user.id);
-
-      const addressFound = await db.address.findOne({
-        where: { store_id, id }
-      });
-      if (!addressFound) {
-        res.status(404).send({ message: "Error: Not Found" });
+      if (store_id !== null) {
+        const addressFound = await db.address.findOne({
+          where: { store_id, id }
+        });
+        if (!addressFound) {
+          res.status(404).send({ message: "Error: Not Found" });
+        } else {
+          await addressFound.destroy();
+          // console.log(address);
+          res.status(200).send({ message: "Delete Success" });
+        }
       } else {
-        const address = await addressFound.destroy();
-        console.log(address);
-        res.status(200).send({ message: "Delete Success" });
+        res.status(401).send({ message: "Unauthorized" });
       }
     }
   );
--- services/bank.js
+++ services/bank.js
@@ -1,9 +1,8 @@
 const passport = require("passport");
-const {findStoreIDbyUserID} = require("../utils");
+const { findStoreIDbyUserID } = require("../utils");
 
 module.exports = (app, db) => {
-  app.get("/bank/:store_id", 
-  async (req, res) => {
+  app.get("/bank/:store_id", async (req, res) => {
     let result = await db.bank.findAll({
       where: {
         store_id: req.params.store_id
@@ -16,68 +15,105 @@ module.exports = (app, db) => {
     }
   });
 
-  app.post("/bank",   
-  passport.authenticate("jwt", { session: false }),
-  async (req, res) => {
-    const store_id = await findStoreIDbyUserID(db, req.user.id);
-    db.bank
-      .create({
-        bank_name: req.body.bank_name,
-        account_name: req.body.account_name,
-        account_number: req.body.account_number,
-        store_id
-      })
-      .then(result => {
-        res.status(201).json(result);
-      })
-      .catch(err => {
-        console.error(err);
-        res.status(400).json({
-          message: err.message
+  app.get(
+    "/bank",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+      const store_id = await findStoreIDbyUserID(db, req.user.id);
+      if (store_id !== null) {
+        let result = await db.bank.findAll({
+          where: {
+            store_id
+          }
         });
-      });
-  });
+        if (!result) {
+          res.status(404).send({ message: "Not found bank by store id" });
+        } else {
+          res.status(200).send(result);
+        }
+      } else {
+        res.status(401).send({ message: "Unauthorized" });
+      }
+    }
+  );
 
-  app.put("/bank/:id",
-  passport.authenticate("jwt", { session: false }),
-   async (req, res) => {
-    const id = req.params.id;
-    const store_id = await findStoreIDbyUserID(db, req.user.id);
-    const bankFound = await db.bank.findOne({
-      where: { store_id, id }
-    });
-    if (!bankFound) {
-      res.status(404).send({ message: "Error: Not Found" });
-    } else {
-      try {
-        const bank = await bankFound.update({
+  app.post(
+    "/bank",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+      const store_id = await findStoreIDbyUserID(db, req.user.id);
+      db.bank
+        .create({
           bank_name: req.body.bank_name,
           account_name: req.body.account_name,
           account_number: req.body.account_number,
+          store_id
+        })
+        .then(result => {
+          res.status(201).json(result);
+        })
+        .catch(err => {
+          res.status(400).json({
+            message: err.message
+          });
         });
-        console.log(bank);
-        res.status(200).send({ message: "Update Success", ...bank.dataValues });
-      } catch (error) {
-        // res.status(400).send({ message: "bank name cannot be empty." })
-        res.status(400).send({ message: error.errors[0].message });
+    }
+  );
+
+  app.put(
+    "/bank/:id",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+      const id = req.params.id;
+      const store_id = await findStoreIDbyUserID(db, req.user.id);
+      if (store_id !== null) {
+        const bankFound = await db.bank.findOne({
+          where: { store_id, id }
+        });
+        if (!bankFound) {
+          res.status(404).send({ message: "Error: Not Found" });
+        } else {
+          try {
+            const bank = await bankFound.update({
+              bank_name: req.body.bank_name,
+              account_name: req.body.account_name,
+              account_number: req.body.account_number
+            });
+            // console.log(bank);
+            res
+              .status(200)
+              .send({ message: "Update Success", ...bank.dataValues });
+          } catch (error) {
+            // res.status(400).send({ message: "bank name cannot be empty." })
+            res.status(400).send({ message: error.errors[0].message });
+          }
+        }
+      } else {
+        res.status(401).send({ message: "Unauthorized" });
       }
     }
-  });
+  );
 
-  app.delete("/bank/:id",
-  passport.authenticate("jwt", { session: false }),
-   async (req, res) => {
-    const id = req.params.id;
-    const store_id = await findStoreIDbyUserID(db, req.user.id);
-    const bankFound = await db.bank.findOne({
-      where: { store_id, id }
-    });
-    if (!bankFound) {
-      res.status(404).send({ message: "Error: Not Found" });
-    } else {
-      const bank = await bankFound.destroy();
-      console.log(bank);
-      res.status(200).send({ message: "Delete Success" });
+  app.delete(
+    "/bank/:id",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+      const id = req.params.id;
+      const store_id = await findStoreIDbyUserID(db, req.user.id);
+      if (store_id !== null) {
+        const bankFound = await db.bank.findOne({
+          where: { store_id, id }
+        });
+        if (!bankFound) {
+          res.status(404).send({ message: "Error: Not Found" });
+        } else {
+          await bankFound.destroy();
+          // console.log(bank);
+          res.status(200).send({ message: "Delete Success" });
+        }
+      } else {
+        res.status(401).send({ message: "Unauthorized" });
+      }
     }
-  });
+  );
 };
